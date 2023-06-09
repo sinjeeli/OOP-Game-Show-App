31b907b96f355b1b7f8dac1551a66d5bed559424
sinjeeli <sinjeeli@gmail.com>
2023-06-09 08:15:34 -0400

    good so far

--- js/Game.js
+++ js/Game.js
@@ -1,26 +1,33 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
-
 class Game {
-
     constructor() {
-
 this.missed = 0,
-
 this.phrases = [
-    
     new Phrase("Hello world"),
     new Phrase("Treehouse is awesome"),
     new Phrase("Coding is fun"),
     new Phrase("JavaScript is powerful"),
     new Phrase("I love programming")
-
-
 ],
-
 this.activePhrase = null;
+    }
+
+    getRandomPhrase() {
+
+        const randomIndex = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomIndex];
+
 
     }
+
+    startGame() {
+        const overlay = document.querySelector('#overlay');
+        overlay.style.display = "none";
+
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+
+
+    }
+
 
 }
--- js/Phrase.js
+++ js/Phrase.js
@@ -9,5 +9,26 @@ this.phrase = phrase.toLowerCase();
 
 
 }
+
+addPhraseToDisplay() {
+    const phraseContainer = document.querySelector('#phrase ul');
+    const letters = this.phrase.split('');
+
+    letters.forEach((letter)=>{
+        const li = document.createElement('li');
+        li.textContent = letter;
+
+        if(letter === ' '){
+            li.className = 'space';
+            } 
+            else {
+                li.className = `hide letter ${letter}`;
+
+            }
+
+phraseContainer.appendChild(li);
+
+    });
+}
     
 }
